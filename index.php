9367ab1e722a2fc09a1ba91fcac83b85a3ddeee5
SRBmartin <79235403+SRBmartin@users.noreply.github.com>
2021-03-14 17:31:51 +0100

    Додавање налог класе

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     session_start();
     include_once "classes/dbh.class.php";
     include_once "classes/header.class.php";
+    include_once "classes/nalog.class.php";
     $_SESSION["page_index_navbar_active"] = 0; //određuje na kojoj je stranici da bi se link u navigaciji promenio
     include_once "inc/header.php";
 ?>
