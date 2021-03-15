3567d4abd51acf9ca518c4108136005fdaebb30c
SRBmartin <79235403+SRBmartin@users.noreply.github.com>
2021-03-15 01:17:59 +0100

    Имплементација ,,модал" за одјаву корисника

--- classes/header.class.php
+++ classes/header.class.php
@@ -181,13 +181,31 @@
                                     <a class="dropdown-item" href="#">Профил</a>
                                     <a class="dropdown-item" href="#">Моје књиге</a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="https://skolskabiblioteka.muharemovic.com/p/logout">Одјави се</a>
+                                    <a class="dropdown-item" data-toggle="modal" data-target="#logout-modal" id="logout-profile">Одјави се</a>
                                     </div>
-                                </li>';
+                                </li></ul>';
                         } //NE ZABORAVI DA DODAS INDEXE
-                echo '</ul>
-                 </div>
+                echo '</div>
                  </nav>';
+                 if(isset($_SESSION["korisnik"])){
+                     echo '<div class="modal fade" id="logout-modal" tabindex="-1" role="dialog" aria-labelledby="logout-modal-h" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h5 class="modal-title" id="logout-modal-h">Одјави ме</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    </div>
+                                    <div class="modal-body">Да ли сте сигурни да желите да се одјавите са свог налога?</div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Одустани</button>
+                                    <button type="button" id="logout-m-button" class="btn btn-primary">Одјави се</button>
+                                    </div>
+                                </div>
+                                </div>
+                        </div>';
+                 }
         }
         
     }
