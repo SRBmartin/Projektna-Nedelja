d3b3d5f12fc867eccce009bd2fa7501e2524f034
SRBmartin <79235403+SRBmartin@users.noreply.github.com>
2021-03-22 00:38:44 +0100

    Додата провера за recaptcha

--- inc/header.php
+++ inc/header.php
@@ -14,7 +14,14 @@
     <link rel="icon" href="https://skolskabiblioteka.muharemovic.com/img/logo.png" sizes="16x16">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://skolskabiblioteka.muharemovic.com/styles/main.css">
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <?php
+        if($_SESSION["page_index_navbar_active"] === 6){
+            echo '<script src="../scripts/signup-capt.js"></script>
+                 <script src="https://www.google.com/recaptcha/api.js?onload=g_recapFunc&render=explicit" async defer></script>';
+        } else{
+            echo '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
+        }
+    ?>
 </head>
 <body>
     <noscript>Ваш интернет прегледач не подржава JavaScript-у. Због тога Вам овај сајт неће бити доступан.</noscript>
@@ -22,5 +29,9 @@
         <?php 
             Header::set_header_carousel();
             Header::set_navbar(); 
+            if(!isset($_SESSION["korisnik"])){
+                echo '<div id="m-target"></div>';
+            }
         ?>
+        
     </div>
