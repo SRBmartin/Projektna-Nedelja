5a4f41b591b4500aa98f0cd3370c0517ae2ed708
SRBmartin <79235403+SRBmartin@users.noreply.github.com>
2021-03-22 00:42:42 +0100

    Почетак регистрационог система

--- p/signup.php
+++ p/signup.php
@@ -4,9 +4,13 @@
         include_once "../classes/dbh.class.php";
         include_once "../classes/header.class.php";
         include_once "../classes/nalog.class.php";
+        include_once "../classes/main.class.php";
         $_SESSION["page_index_navbar_active"] = 6; //određuje na kojoj je stranici da bi se link u navigaciji promenio
         include_once "../inc/header.php";
-        echo '</body>';
+        echo '<div class="sUContainer mx-auto">';
+        echo Main::set_signup();
+        echo '</div>
+             </body>';
         include_once "../inc/footer.php";
     } else{
         header("Location: https://skolskabiblioteka.muharemovic.com");
