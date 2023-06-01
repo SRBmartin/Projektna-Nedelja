16511d2efd5c56c62e1fae485935238ced745a9d
SRBmartin <79235403+SRBmartin@users.noreply.github.com>
2023-06-01 23:05:12 +0200

    Updated

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -5,8 +5,6 @@
         private static $username = "root";
         private static $password = "";
         private static $dbname = "projekat";
-
-        //vGq~y(+W&8j5
         public static function connect()
         {
             $conn = new mysqli(self::$servername,self::$username,self::$password,self::$dbname);
